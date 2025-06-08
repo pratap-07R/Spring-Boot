a1c817d3c5fda5be5b761f3543949bcaf1941c4f
Pratap Singh <ranaprataps189@gmail.com>
2025-06-08 12:14:08 +0530

    here are Spring RoadMap

---
+++ roadmap/Roadmap.java
@@ -0,0 +1,98 @@
+// package Spring-Boot.roadmap;
+
+// public class roadmap {
+    
+// }
+
+/*
+1. INTRODECTION TO SPRING BOOT 
+    -> what problem it solved 
+    -> its feature and advantage
+
+2. SETTING UP INITIAL SPRING BOOT PROJECT 
+    ->  Understand layared architacture 
+
+3. UNDERSTAND "MAVEN" , "POM.XML" AND "GRADLE" STRUCTURE A BIT 
+
+4. SPRING BOOT ANNOTATIONS
+    -> @springBootApplication
+    -> @Controler
+    -> RestControler
+    -> @RequestMapping
+    -> @GetMapping, @Postmapping, @PutMapping
+    -> @DeletMapping
+    -> @AutoWired
+    -> @component
+    -> @Service
+    -> @Repositary
+    -> @ComponentScan
+    -> @Configration
+    -> @Value
+    -> @Qualifier
+    -> @Profile
+    -> EnableAutoConfigration
+    -> @Entity
+    -> @Transactional
+    -> EnableCaching
+    -> Async
+    -> EnableScheduling etc
+
+5. UNDERSTAND ABOUT Dependency injection and Beans
+
+6. Spring boot Data Access
+    -> spring JPA
+    -> spring JDBC
+    -> Query method
+
+7. Restful api with spring boot
+
+8. spring boot  Data Access
+    -> authentication and authorisation
+
+9. Spring Boot logging
+
+10. Spring Boot exception halding
+
+11. Spring boot Interceptor
+
+12. Spring Boot Scheduling
+
+13. Spring Boot Caching
+
+14. Spring Boot Unit testing
+    ->Mockito
+
+15. Microservice topic with spring boot
+    ->introduction to microservices
+
+    -> service discovery using eureka
+        -> microservice registring
+    ->tracing the request through many multiple microservices
+        -> Sleuth and Zepkin
+        -> Sleuth we can attach the trace id with request 
+        -> Zepkin help to visualize the trace 
+    ->Spring Boot Profile
+        -> Diffrent properties based on environment
+
+    -> Spring Cloud Configure Service 
+        -> it's a configuration managment
+    
+    -> Comunication between diffrent microservices 
+        -> Sync
+        -> Async
+    
+    -> API Gateway
+
+    -> Circuit Breaker 
+
+    -> CQRS (command query resposibility segregation)
+
+    -> deployment and Containerization
+        -> creating executable JARs and WARs
+        -> Deploying to docker container 
+        
+16. BUILD PROJECT
+
+
+
+ */
